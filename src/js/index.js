4c156f559cb00f014ac23f7189036f8b3fb981a6
Renan Souza de Lima <137123763+Naanz96@users.noreply.github.com>
2024-10-14 01:46:36 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+const listaSelecaoPokedevs = document.querySelectorAll(".pokedev");
+console.log(listaSelecaoPokedevs);
+
+listaSelecaoPokedevs.forEach(pokedev => {
+    pokedev.addEventListener("click", () => {
+        
+        esconderCartaoPokedev();
+        
+        const idPokedevSelecionado = mostrarCartaoPokedev(pokedev);
+
+        desativarPokedevNaListagem();
+
+        ativarPokedevSelecionadoNaListagem(idPokedevSelecionado);
+    })
+})
+
+function ativarPokedevSelecionadoNaListagem(idPokedevSelecionado) {
+    const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado);
+    pokedevSelecionadoNaListagem.classList.add("ativo");
+}
+
+function desativarPokedevNaListagem() {
+    const pokedevAtivoNaListagem = document.querySelector(".ativo");
+    pokedevAtivoNaListagem.classList.remove("ativo");
+}
+
+function mostrarCartaoPokedev(pokedev) {
+    const idPokedevSelecionado = pokedev.attributes.id.value;
+
+    const idCartaoPokedevParaAbrir = "cartao-" + idPokedevSelecionado;
+
+    const cartaoPokedevParaAbrir = document.getElementById(idCartaoPokedevParaAbrir);
+    cartaoPokedevParaAbrir.classList.add("aberto");
+    return idPokedevSelecionado;
+}
+
+function esconderCartaoPokedev() {
+    const cartaoPokedevAberto = document.querySelector(".aberto");
+    cartaoPokedevAberto.classList.remove("aberto");
+}
